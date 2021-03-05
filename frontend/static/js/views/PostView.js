dedb5e8b9ae5665bb29e054ec137e631a2db4efd
XlentAcademy <xlentclass@gmail.com>
2021-03-05 23:29:10 +0530

    Xlent 2.O

--- frontend/static/js/views/PostView.js
+++ frontend/static/js/views/PostView.js
@@ -20,19 +20,6 @@ export default class extends AbstractView {
             `;
         } else if (this.postId == 2) {
             return `
-            <header>
-            <a href="/" class="logo" data-link>Xlent</a>
-            <div class="menu-toggle"></div>
-            <nav>
-                <ul>
-                    <li><a href="/" data-link>Home</a></li>
-                    <li><a href="/posts" class="active" data-link>Posts</a></li>
-                    <li><a href="/settings" data-link>Settings</a></li>
-                </ul>
-            </nav>
-            <div class="clearfix"></div>
-        </header>
-        <br /><br /><br /><br /><br /><br /><br /><br />
         <div class="maintext">
             <h1>Posts</h1>
             <p>Welcome to Xlent Academy!</p>
@@ -42,23 +29,10 @@ export default class extends AbstractView {
             `;
         } else {
             return `
-            <header>
-            <a href="/" class="logo" data-link>Xlent</a>
-            <div class="menu-toggle"></div>
-            <nav>
-                <ul>
-                    <li><a href="/" data-link>Home</a></li>
-                    <li><a href="/posts" class="active" data-link>Posts</a></li>
-                    <li><a href="/settings" data-link>Settings</a></li>
-                </ul>
-            </nav>
-            <div class="clearfix"></div>
-        </header>
-        <br /><br /><br /><br /><br /><br /><br /><br />
         <div class="maintext">
             <h1>Posts</h1>
             <p>Welcome to Xlent Academy!</p>
-            <p>The post is not Here!.</p>
+            <p>This post does not exits!.</p>
             <a data-link>Go Back</a>
         </div>
             `;

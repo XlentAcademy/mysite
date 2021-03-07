300314836408d911a28af98286c6b6c625b0aae0
XlentAcademy <xlentclass@gmail.com>
2021-03-07 16:13:14 +0530

    Xlent 2.O

--- frontend/static/js/views/Test.js
+++ frontend/static/js/views/Test.js
@@ -3,7 +3,7 @@ import AbstractView from "./AbstractView.js";
 export default class extends AbstractView {
     constructor(params) {
         super(params);
-        this.setTitle("Xlent Academy-Settings");
+        this.setTitle("Xlent Academy-Test");
     }
 
     async getHtml() {
@@ -11,7 +11,7 @@ export default class extends AbstractView {
     <div class="maintext">
         <p>Welcome to Xlent Academy!</p>
         <!-- <embed class="pdfsh" src="/static/test.pdf" width="100%" height="700px" data-link> </embed> -->
-        <object class="pdfsh" data="/static/xlent_test.pdf" type="application/pdf" width="100%" height="700px" data-link>
+        <object class="pdfsh" data="https://docs.google.com/gview?embedded=true&url=https://xlentacademy.tk/12th_jee_1.pdf" type="application/pdf" width="100%" height="700px" data-link>
         <p style="font-size: 4px; color: grey;">Your web browser doesn't have a PDF plugin.
         Instead you can <a href="https://xlentacademy.tk/12th_jee_1.pdf" download="xlent_test.pdf">click here to
         download the PDF file.</a></p>

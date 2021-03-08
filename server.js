0e6b8826624e6045340598d73bb45230b4e56bf8
XlentAcademy <xlentclass@gmail.com>
2021-03-08 18:07:29 +0530

    new

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ const app = express();
 app.use("/static", express.static(path.resolve(__dirname, "frontend", "static")));
 
 app.get("/*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "frontend", "index.html"));
+    res.sendFile(path.resolve(__dirname, "frontend", "static", "php", "index.php"));
 })
 
 app.listen(process.env.PORT || 5000, () => console.log("Server running on port 5000"));

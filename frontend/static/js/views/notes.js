097c885d8b1ead77d7c9c7fca001037c678bab91
XlentAcademy <xlentclass@gmail.com>
2021-03-15 23:11:19 +0530

    new tabs

--- frontend/static/js/views/notes.js
+++ frontend/static/js/views/notes.js
@@ -8,8 +8,11 @@ export default class extends AbstractView {
 
     async getHtml() {
         return `
+        <center><div id="loader"></div></center>
+        <div style="display:none;" id="myDiv" class="animate-bottom">
         <div class="maintext">
-            <img width="100%" height="100%" src="/static/components/study/chart.svg" />
+            <img onload="timeout()" width="100%" height="100%" src="/static/components/study/chart.svg" />
+        </div>
         </div>
         `;
     }
